defb136db86138344dbc39641836ad21075b9b24
songrongjie <1554019387@qq.com>
2020-10-08 19:25:03 +0800

    引入全局sass

--- src/views/doctor/subscribe.vue
+++ src/views/doctor/subscribe.vue
@@ -17,6 +17,8 @@ export default {
 
 <style lang="scss" scoped>
   .subscribe{
-
+    h1 {
+      color: $primary-deep;
+    }
   }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 
-function resolve(dir) {
+function resolve (dir) {
   return path.join(__dirname, dir)
 }
 module.exports = {
@@ -18,6 +18,14 @@ module.exports = {
         'resolve url': true,
         // 自定义主题场景
         import: [path.resolve(__dirname, './src/assets/css/base.styl')]
+      },
+      sass: {
+        // 向全局sass样式传入共享的全局变量, $src可以配置图片cdn前缀
+        // 详情: https://cli.vuejs.org/guide/css.html#passing-options-to-pre-processor-loaders
+        prependData: `
+        @import "@/assets/scss/variable.scss";
+        @import "@/assets/scss/mixin.scss";
+        `
       }
     }
   }
